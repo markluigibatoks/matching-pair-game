7d7e01b4a5ccc4b293f47a79c680d501d2caa07c
Mark-Luigi <markluigibatoks@gmail.com>
2026-01-17 11:18:26 -0330

    add: improved design and added flip and value props

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -1,15 +1,21 @@
-export default function Card () {
+export default function Card ({
+  value = "",
+  isFlipped = false
+}: {
+  value: string,
+  isFlipped: boolean
+}) {
   return (
-    <div className="relative perspective-normal w-24 aspect-square">
-      <div className="hover:rotate-y-180 absolute w-full h-full shadow rounded border-2 border-white transition duration-1000 transform-3d">
+    <div className={`${isFlipped ? 'cursor-pointer' : ''} relative perspective-normal w-24 aspect-square select-none`}>
+      <div className={`${isFlipped ? "rotate-y-180" : ""} absolute w-full h-full shadow rounded border-2 border-white transition duration-1000 transform-3d`}>
         {/* Front */}
-        <div className="absolute w-full h-full bg-white text-red-100 text-center text-4xl rounded-md backface-hidden">
-          Front
+        <div className="absolute w-full h-full bg-white text-red-700 text-center text-4xl rounded-lg backface-hidden flex justify-center items-center">
+          ?
         </div>
 
         {/* Back */}
-        <div className="absolute w-full h-full bg-amber-50 text-red-100 text-center text-4xl rounded-md backface-hidden rotate-y-180">
-          Back
+        <div className="absolute w-full h-full bg-amber-50 text-center text-4xl rounded-lg backface-hidden rotate-y-180 flex justify-center items-center [text-shadow:-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,1px_1px_0_#000]">
+          {value}
         </div>
       </div>
     </div>
