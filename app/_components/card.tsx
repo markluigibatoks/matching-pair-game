250b0e7b652fafdb72341567e9d3b045c4010303
Mark-Luigi <markluigibatoks@gmail.com>
2026-01-25 08:46:22 -0330

    refactor: card size

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -6,7 +6,7 @@ export default function Card ({
   isFlipped: boolean
 }) {
   return (
-    <div className={`${isFlipped ? 'cursor-pointer' : ''} relative perspective-normal w-24 aspect-square select-none`}>
+    <div className={`${isFlipped ? 'cursor-pointer' : ''} relative perspective-normal w-16 aspect-square select-none`}>
       <div className={`${isFlipped ? "rotate-y-180" : ""} absolute w-full h-full shadow rounded border-2 border-white transition duration-1000 transform-3d`}>
         {/* Front */}
         <div className="absolute w-full h-full bg-white text-red-700 text-center text-4xl rounded-lg backface-hidden flex justify-center items-center">
