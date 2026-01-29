269238ad84f90df7dd610ece3fd616fe4264f4d9
Mark-Luigi <markluigibatoks@gmail.com>
2026-01-28 21:58:24 -0330

    add: store file

--- app/_lib/store.tsx
+++ app/_lib/store.tsx
@@ -2,10 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 
 //reducer
 import startGameSliceReducer from './feat/startGame/startGame.slice'
+import flipCardSliceReducer from './feat/flipCard/flipCard.slice'
+import fetchGameReducer from './feat/fetchGame/fetchGame.slice'
 
 export const makeStore = configureStore({
   reducer: {
-    startGameSlice: startGameSliceReducer
+    startGameSlice: startGameSliceReducer,
+    flipCardSlice: flipCardSliceReducer,
+    fetchGameSlice: fetchGameReducer,
   },
 });
 
